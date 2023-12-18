2e7f5419fb462298bf1a714ae5c9f2609637be41
DavidH49 <david.heilmannseder@cometomail.de>
2023-12-18 08:29:33 +0100

    Cleaned up with clippy

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,7 @@ fn parse_path(path: &str) -> String {
     static RE_FILEPATH: Lazy<Regex> = Lazy::new(|| Regex::new(REGEX_FILEPATH).unwrap());
 
     // Replace file extension
-    let dst_path = RE_FILEEXT.replace(&path, EXTENSION);
+    let dst_path = RE_FILEEXT.replace(path, EXTENSION);
 
     // Replace file path
     let dst_path = RE_FILEPATH.replace(&dst_path, DST_PATH);
